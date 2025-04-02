a28442fce5c2f0555f3e515ebeeb46515d0bddbd
Nanda gunaratne <34383380+nandagunaratne@users.noreply.github.com>
2025-04-02 13:47:55 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+$title = 'Home - Welcome to TransAm Piping Products';
+$is_active = 'home';
+require_once 'content/navigation/header.php';
+?>
+
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+	<div id="heroCarousel" class="carousel slide carousel-fade"
+		data-ride="carousel">
+		<div class="carousel-inner" role="listbox">
+							    <!-- Slide 1 -->
+			<!-- 				    
+			<div class="carousel-item responsive active"
+				style="background-image: url(assets/img/slide/HolidayHours.jpg);">
+			</div> 
+			-->
+			<!-- Slide 1 --> 
+			<div class="carousel-item responsive active"
+				style="background-image: url(assets/img/slide/CalgaryLocation.jpg);">
+			</div>
+			<!-- Slide 2 -->
+			<div class="carousel-item responsive"
+				style="background-image: url(assets/img/slide/CalgaryYard2.jpg);">
+			</div>
+			<!-- Slide 3 -->
+			<div class="carousel-item responsive"
+				style="background-image: url(assets/img/slide/warehouse3.jpg);">
+			</div>
+		  <!-- Slide 4 -->
+			<div class="carousel-item responsive"
+				style="background-image: url(assets/img/slide/Warehouse6.jpg);">
+			</div>
+
+		</div>
+		<a class="carousel-control-prev" href="#heroCarousel" role="button"
+			data-slide="prev"> <span
+			class="carousel-control-prev-icon bx bx-left-arrow"
+			aria-hidden="true"></span> <span class="sr-only">Previous</span>
+		</a> <a class="carousel-control-next" href="#heroCarousel"
+			role="button" data-slide="next"> <span
+			class="carousel-control-next-icon bx bx-right-arrow"
+			aria-hidden="true"></span> <span class="sr-only">Next</span>
+		</a>
+		<ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+	</div>
+</section>
+<!-- End Hero -->
+<main id="main"> <!-- ======= Cta Section ======= -->
+<section id="cta" class="cta">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-9 text-center text-lg-left">
+				<h3>Welcome to Trans Am Piping Products Ltd.!</h3>
+				<p>&nbsp;</p>
+				<p>Based in Alberta, Canada, Trans Am has been a distributor of high
+					quality carbon steel piping components since 1975.</p>
+				<p>&nbsp;</p>
+				<p>With sales centers in Calgary and Edmonton, Trans Am offers the
+					most complete inventory of Buttweld Fittings, Forged Fittings,
+					Forged Flanges, Pipe and related Piping components available in western
+					Canada, with an experienced staff to support your needs.</p>
+				<p>&nbsp;</p>
+				<p>Specialists in Carbon Steel Pipe, Flanges, Fittings and related piping components.</p>
+			</div>
+			<div class="text-top col-lg-3 text-right text-lg-right">
+				<div class="col-lg-3  text-center">
+					<a class="cta-btn align-middle" target="_blank"
+						href="assets/pdf/iso.pdf"><img
+						src="assets/img/ISO_9001_Quality_MONO.jpg" width="250"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- End Cta Section -->	
+
+
+<!-- Start Temporary for XMas Section -- Also need to enable the script on bottom of page-->
+<!-- 
+<div id="simpleModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Merry Christmas and Happy New Year</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <iframe src="assets/img/Merry Christmas and Happy New Year 2023.png" width="750" height="850"frameborder = "0" ></iframe>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div> 
+
+
+ -->
+<!-- End Temporary for XMas Section -->
+
+
+
+<?php
+
+require_once 'content/navigation/footer.php';
+?>
+
+<!-- Start Temporary for XMas Section -->
+<!-- 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    window.onload = function () {
+        OpenBootstrapPopup();
+    };
+    function OpenBootstrapPopup() {
+        $("#simpleModal").modal('show');
+    }
+</script> 
+ -->
+<!-- End Temporary for XMas Section -->
